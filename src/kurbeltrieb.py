754ad809b23914b83103ae78282e3cb3037049c2
honzatomek <jan.tomek@protonmail.com>
2024-02-16 17:25:21 +0100

    added PyQt5 dependency and fixed logging setup

--- src/kurbeltrieb.py
+++ src/kurbeltrieb.py
@@ -23,6 +23,7 @@ import numpy as np
 import sympy as sm
 import sympy.physics.mechanics as me
 
+import matplotlib
 import matplotlib.pyplot as plt
 
 #------------------------------------------------------------- GENERAL SETUP ---#
@@ -32,6 +33,56 @@ PLOT_SIZE = (16, 10)
 _NOW = datetime.datetime.strftime(datetime.datetime.now(), "%Y%m%d_%H%M%S")
 
 #------------------------------------------------------------- LOGGING SETUP ---#
+def addLoggingLevel(levelName, levelNum, methodName=None):
+    """from: https://stackoverflow.com/questions/2183233/how-to-add-a-custom-loglevel-to-pythons-logging-facility
+    Comprehensively adds a new logging level to the `logging` module and the
+    currently configured logging class.
+
+    `levelName` becomes an attribute of the `logging` module with the value
+    `levelNum`. `methodName` becomes a convenience method for both `logging`
+    itself and the class returned by `logging.getLoggerClass()` (usually just
+    `logging.Logger`). If `methodName` is not specified, `levelName.lower()` is
+    used.
+
+    To avoid accidental clobberings of existing attributes, this method will
+    raise an `AttributeError` if the level name is already an attribute of the
+    `logging` module or if the method name is already present
+
+    Example
+    -------
+    >>> addLoggingLevel('TRACE', logging.DEBUG - 5)
+    >>> logging.getLogger(__name__).setLevel("TRACE")
+    >>> logging.getLogger(__name__).trace('that worked')
+    >>> logging.trace('so did this')
+    >>> logging.TRACE
+    5
+
+    """
+    if not methodName:
+        methodName = levelName.lower()
+
+    if hasattr(logging, levelName):
+       raise AttributeError('{} already defined in logging module'.format(levelName))
+    if hasattr(logging, methodName):
+       raise AttributeError('{} already defined in logging module'.format(methodName))
+    if hasattr(logging.getLoggerClass(), methodName):
+       raise AttributeError('{} already defined in logger class'.format(methodName))
+
+    # This method was inspired by the answers to Stack Overflow post
+    # http://stackoverflow.com/q/2183233/2988730, especially
+    # http://stackoverflow.com/a/13638084/2988730
+    def logForLevel(self, message, *args, **kwargs):
+        if self.isEnabledFor(levelNum):
+            self._log(levelNum, message, args, **kwargs)
+    def logToRoot(message, *args, **kwargs):
+        logging.log(levelNum, message, *args, **kwargs)
+
+    logging.addLevelName(levelNum, levelName)
+    setattr(logging, levelName, levelNum)
+    setattr(logging.getLoggerClass(), methodName, logForLevel)
+    setattr(logging, methodName, logToRoot)
+
+
 _LOG_FORMATTER = logging.Formatter(fmt=" %(asctime)13s *%(levelname).1s* %(message)s",
                                    datefmt="%y%m%d-%H%M%S", style="%")
 _OFFSET = 21
