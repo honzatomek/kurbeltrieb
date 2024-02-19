efc07e8e47d428e7cce32cbab1da8f3b2c51d029
honzatomek <jan.tomek@protonmail.com>
2024-02-19 23:50:19 +0100

    harmonic decomposition of gasdata

--- src/gasdata.py
+++ src/gasdata.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import os
 import sys
 from typing import Union
@@ -87,8 +89,78 @@ def write_gasdata(filename: str, gasdata: np.ndarray):
 
 
 
+def harmonic_decomp(t: np.ndarray, y: np.ndarray) -> np.ndarray:
+    """Harmonic decomposition of a time signal to a combination of sines
+    and cosines, such that:
+
+    y(t) = Σ (A_i cos(2 π f_i t) + B_i sin(2 π f_i t))
+
+    Args:
+        t (np.ndarray): time vector
+        y (np.ndarray): signal data, if the time step is not uniform, will be resampled
+
+    Returns:
+        (np.ndarray): [f, A, B]
+    """
+    N  = t.shape[0]
+    T  = t[-1] - t[0]
+    fs = 1 / T
+    dt = t[1:] - t[:-1]
+
+    # is timestep uniform?
+    if np.isclose(np.min(dt), np.max(dt)):  # yes
+        dt = np.mean(dt[0])
+    else:                                   # no -> resample
+        print(f"[i] Resampling data to uniform timestep.")
+        dt = T / (N - 1)
+        f = scipy.interpolate.interp1d(t, y)
+        t = np.arange(t[0], t[-1], dt)
+        y = f(t)
+
+    midpoint = N // 2
+    fft = scipy.fft.fft(y)[:midpoint] * (2.0 / N)
+    fft[0] /= 2.
+    frq = scipy.fft.fftfreq(N, dt)[:midpoint]
+
+    res = []
+    for f, A in zip(frq, fft):
+        res.append([f, A.real, -A.imag])
+
+    return np.array(res, dtype=float)
+
+
+def harmonic_comp(t: np.ndarray, fft: np.ndarray):
+    res = np.zeros(t.shape[0])
+    for f, A, B in fft:
+        res += A * np.cos(2. * np.pi * f * t) + B * np.sin(2. * np.pi * f * t)
+
+    return res
+
+
+
 if __name__ == "__main__":
+    # gd = create_gasdata()
+    # plot_gasdata(gd)
+    # write_gasdata("gasdata_rnd.asc", gd)
+
     gd = create_gasdata()
-    plot_gasdata(gd)
-    write_gasdata("gasdata_rnd.asc", gd)
+    angle, pressure = gd[:, 0], gd[:, 1]
+    rpm = 13000.
+    omega = rpm / 60. * 2 * np.pi
+    radians = np.radians(angle)
+    time = radians / omega
+
+    fft = harmonic_decomp(time, pressure)
+
+    comp = harmonic_comp(time, fft[1:7,:])
+
+    fig = plt.figure()
+    ax  = fig.add_subplot(111)
+
+    ax.plot(time, pressure, label="orig")
+    ax.plot(time,     comp, label="comp")
+    ax.legend()
+
+    plt.show()
+
 
